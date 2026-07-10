cef190f3654a4c05e59a91908ba9ed8ec48c9919
Saroj Chettri <saroj.chettri@MacbookAir.local>
2026-07-10 14:18:25 +0545

    Prisma migration for production case settled

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,11 +4,6 @@ set -e
 echo "Generating Prisma client..."
 uv run prisma generate
 
-echo "Running Prisma migrations..."
-
-echo "Running Prisma migration dev fro crate migrations file..."
-uv run prisma migrate dev
-
 echo "Deploying Prisma migrations..."
 uv run prisma migrate deploy
 
