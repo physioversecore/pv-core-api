0c6bc81b7e70a8c140e74fe2354c68ae9a4ad436
Saroj Chettri <saroj.chettri@MacbookAir.local>
2026-08-17 16:07:31 +0545

    Patch: adjusted docker-entrypoint for render deployments

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -7,14 +7,20 @@ uv run prisma generate
 echo "Checking Prisma migration status..."
 uv run prisma migrate status
 
+echo "Sleep for 5 sec "
+sleep 5
+
 echo "Applying pending Prisma migrations..."
 uv run prisma migrate deploy
 
-echo "Checking Prisma migration status after deploy..."
-uv run prisma migrate status
+echo "Sleep for 5 sec "
+sleep 5
 
 echo " Seeding some mock information"
 uv run scripts/seed-all.py
+
+echo "Sleep for 5 sec "
+sleep 5
 
 echo "Starting application..."
 uv run main.py
